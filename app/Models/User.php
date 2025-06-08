7cf7608e096b240a04084dd75c35e2c32767568d
youssefsafwat234 <yousefsafwat367@gmail.com>
2025-06-09 01:56:15 +0300

    the new artisan command to generate the configuration files 1-` php artisan config:publish` => this will give you a list to choose which configuration file you want to public 2- `php artisan config:publish --all` will publish all the configuration files like laravel 10 except the sanctum configuration file

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,11 +23,6 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
